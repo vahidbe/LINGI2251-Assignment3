5a485c5520a3a30fea442a9686d4a9684e1b1f28
Vahid Beyraghi <38432333+vahidbe@users.noreply.github.com>
2021-04-22 12:06:13 +0200

    lab4

---
+++ SQA-lab4-source/philosophe/DiningPhilosophers.java
@@ -0,0 +1,26 @@
+package philosophe;
+
+/*
+ * Cette classe est le point d'entrée du programme.
+ */
+public class DiningPhilosophers {
+
+    // Le nombre de philosophe.
+    public static final int NUM_PHILS = 2;
+
+    public static void main(final String[] args) {
+
+        // Création des fourchettes
+        final Fork[] forks = new Fork[NUM_PHILS];
+        for (int i = 0; i != NUM_PHILS; i++) {
+            forks[i] = new Fork(i);
+        }
+
+        // Création des philosophes.
+        final Philosopher[] philosophers = new Philosopher[NUM_PHILS];
+        for (int i = 0; i != NUM_PHILS; i++) {
+            philosophers[i] = new Philosopher(i, forks[i], forks[(i + 1) % NUM_PHILS]);
+            philosophers[i].start();
+        }
+    }
+}
---
+++ SQA-lab4-source/philosophe/Fork.java
@@ -0,0 +1,51 @@
+package philosophe;
+
+
+/*
+ * Cette classe modélise une fourchette.
+ */
+public final class Fork {
+
+    private static final Object lock = new Object();
+
+    private boolean free;
+
+    private final int num;
+
+    public Fork(final int n) {
+        free = true;
+        num = n;
+    }
+
+
+    /*
+     * Cette méthode simule la saisie de la fourchettes.
+     */
+    public void grab() {
+        synchronized (lock) {
+            try {
+                if (!free) {
+                    wait();
+                }
+                free = false;
+            } catch (final InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /*
+     * Cette méthode simule le relachement de la fourchette.
+     */
+    public void release() {
+        synchronized (lock) {
+            free = true;
+            notifyAll();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "FORK num =" + num;
+    }
+}
